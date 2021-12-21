20a9ad6016ab9f59a229700f3602d106e49cfed9
laylapie22 <laylamira@gmail.com>
2021-12-21 11:40:45 +0800

    added Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './heroes/hero';
-import { HEROES } from './mock-heroes';
+import { HEROES } from './mock-heroes';     
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 
   constructor() { }
